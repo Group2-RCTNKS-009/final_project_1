0bd16111c8838af6f119d57484aa160bbcfc321f
farhatsaputra <muhammad.saputra@autopedia.id>
2023-10-19 20:38:59 +0700

    menambahkan Navbar dan edit wording di index.html

---
+++ Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <>
+      <h3>Hello World</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente
+        voluptatum est saepe explicabo temporibus ea, mollitia, dignissimos,
+        minus consequuntur atque quia. Modi voluptatibus atque incidunt
+        perferendis laudantium quos quam velit.
+      </p>
+    </>
+  );
+}
+
+export default Navbar;
